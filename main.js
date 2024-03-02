c1b7329ce1297075b5ca3b53596056ad3cba6418
janita tauqeer <janitatauqeer17@gmail.com>
2024-03-02 13:44:44 -0800

    second assignment

---
+++ main.js
@@ -0,0 +1,29 @@
+//example-One (String)
+var myname = "Janita";
+console.log(myname);
+var lastname = "Tauqeer";
+console.log(lastname);
+//example-Two (Number)
+var a = 17;
+var b = 25;
+console.log("a = 17 and b = 25");
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+//example-Three (Boolean)
+var isfemale = true;
+console.log(isfemale);
+var ismale = false;
+console.log(ismale);
+//example-Four (Null)
+var nullnumber = null;
+console.log(nullnumber);
+//example-Five (Undefined)
+var A;
+console.log(A);
+//example-Six (Symbol)
+var mysymbol = { symbol: "Mj" };
+console.log(mysymbol);
+//examle-Seven (Bigint)
+var bigint = (17777777777777777777777777);
+console.log(bigint);
---
+++ main.ts
@@ -0,0 +1,37 @@
+//example-One (String)
+let myname:string="Janita";
+console.log(myname);
+
+let lastname:string="Tauqeer";
+console.log(lastname);
+
+//example-Two (Number)
+let a:number =17;
+let b:number =25;
+console.log("a = 17 and b = 25");
+console.log(a+b);
+console.log(a-b);
+console.log(a*b)
+
+//example-Three (Boolean)
+let isfemale:boolean =true;
+console.log(isfemale);
+
+let ismale:boolean =false;
+console.log(ismale);
+
+//example-Four (Null)
+let nullnumber:null =null;
+console.log(nullnumber);
+
+//example-Five (Undefined)
+let A;
+console.log(A);
+
+//example-Six (Symbol)
+let mysymbol ={symbol:"Mj"};
+console.log(mysymbol);
+
+//examle-Seven (Bigint)
+let bigint=(17777777777777777777777777);
+console.log(bigint);
